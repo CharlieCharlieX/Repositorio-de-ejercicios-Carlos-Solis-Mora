64ec33df2d6da840a35401c8365b90b10617f4f3
CharlieCharlieX <125424505+CharlieCharlieX@users.noreply.github.com>
2023-03-25 07:14:13 -0300

    Update Program.fs

--- Paradigma Funcional(Tareas #2)/Ejercicio #2/Program.fs
+++ Paradigma Funcional(Tareas #2)/Ejercicio #2/Program.fs
@@ -1,5 +1,10 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 
+(*
+Trabajo realizado por Carlos Eduardo Solís Mora.
+Carné: 2021051459
+*)
+
 //Esta función se encarga de generar una lista, creando en la primera posición el primer número que se pasa por parámetro
 //Y la última posición corresponde al segundo numero pasado por parámetro. Luego se llama de manera recursiva las posiciones
 //intermedias sumandole uno al primer valor hasta que se complete la lista.
